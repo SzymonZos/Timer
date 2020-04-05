3fab2e37dbf0cba3f3baf4ebce6b13eb1a321334
Szymon Zosgornik <szymon.zosgornik@gmail.com>
2020-04-05 19:56:08 +0200

    Add timer v 0.1.0

---
+++ Timer.hpp
@@ -0,0 +1,80 @@
+#ifndef BASICCPPTHREADS_TIMER_HPP
+#define BASICCPPTHREADS_TIMER_HPP
+
+#include <chrono>
+#include <iostream>
+
+template<typename T = std::chrono::milliseconds>
+class Timer {
+public:
+    Timer() : startTimePoint_{std::chrono::high_resolution_clock::now()} {}
+
+    ~Timer() {
+        StopTimer();
+    }
+
+    Timer(const Timer& other) = delete;
+    Timer& operator=(const Timer& other) = delete;
+
+    Timer(Timer&& other) = delete;
+    Timer& operator=(Timer&& other) = delete;
+
+private:
+    std::chrono::time_point<std::chrono::high_resolution_clock>
+        startTimePoint_;
+
+    void StopTimer() {
+        const auto endTimePoint = std::chrono::high_resolution_clock::now();
+        const auto start = std::chrono::time_point_cast<T>(startTimePoint_)
+                               .time_since_epoch()
+                               .count();
+        const auto end = std::chrono::time_point_cast<T>(endTimePoint)
+                             .time_since_epoch()
+                             .count();
+        PrintTime(start, end);
+    }
+
+    template<typename Count>
+    void PrintTime(Count start, Count end);
+};
+
+template<>
+template<typename Count>
+void Timer<std::chrono::nanoseconds>::PrintTime(Count start, Count end) {
+    std::cout << "Elapsed time: "
+              << static_cast<double>((end - start)) / 1000.0 << " us  \n";
+}
+
+template<>
+template<typename Count>
+void Timer<std::chrono::microseconds>::PrintTime(Count start, Count end) {
+    std::cout << "Elapsed time: "
+              << static_cast<double>((end - start)) / 1000.0 << " ms  \n";
+}
+
+template<>
+template<typename Count>
+void Timer<std::chrono::milliseconds>::PrintTime(Count start, Count end) {
+    std::cout << "Elapsed time: "
+              << static_cast<double>((end - start)) / 1000.0 << " s  \n";
+}
+
+template<>
+template<typename Count>
+void Timer<std::chrono::seconds>::PrintTime(Count start, Count end) {
+    std::cout << "Elapsed time: " << end - start << " s  \n";
+}
+
+template<>
+template<typename Count>
+void Timer<std::chrono::minutes>::PrintTime(Count start, Count end) {
+    std::cout << "Elapsed time: " << end - start << " min  \n";
+}
+
+template<>
+template<typename Count>
+void Timer<std::chrono::hours>::PrintTime(Count start, Count end) {
+    std::cout << "Elapsed time: " << end - start << " h  \n";
+}
+
+#endif // BASICCPPTHREADS_TIMER_HPP
